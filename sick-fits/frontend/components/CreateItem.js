1ce14d41814229d352b321f3f2d5ccfaea206ad2
new <new@new10s-MacBook-Pro.local>
2019-02-25 09:23:36 -0800

    add eslint, code broken

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,23 +1,102 @@
-import React, { Component } from 'react'
-import { Mutation } from 'react-apollo'
-import gql from 'graphql-tag'
-import Form from './styles/Form'
-import formatMoney from '../lib/formatMoney'
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Form from "./styles/Form";
+// import formatMoney from "../lib/formatMoney"
+import Error from "./ErrorMessage";
 
+const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
+    ) {
+      id
+    }
+  }
+`;
 
 class CreateItem extends Component {
+  state = {
+    title: "cool",
+    description: "",
+    image: "",
+    largeImage: "",
+    price: 0
+  };
+
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type === "number" ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+
   render() {
+    const { title, price, description, image, largeImage } = this.state;
     return (
-      <Form>
-        <fieldset>
-          <label htmlFor="title">
-            Title 
-            <input type="text" id="title" name="title" placeholder="Title" required/>
-          </label>
-        </fieldset>
-      </Form>
-    )
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, { loading, error }) => (
+          <Form
+            onSubmit={e => {
+              e.preventDefault();
+              console.log("testert")
+              console.log(this.state);
+            }}
+          >
+            <Error error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  value={title}
+                  onChange={this.handleChange}
+                  required
+                />
+              </label>
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  value={price}
+                  onChange={this.handleChange}
+                  required
+                />
+              </label>
+              <label htmlFor="description">
+                Description
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter a description"
+                  value={description}
+                  onChange={this.handleChange}
+                  required
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
+    );
   }
 }
 
-export default CreateItem
+export default CreateItem;
+export { CREATE_ITEM_MUTATION };
